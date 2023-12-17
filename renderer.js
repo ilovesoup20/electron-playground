9b7373ad8ef22f819ecaecd8370f60842d6ed9d7
Charles Chong <ilovesoup@fastmail.com>
2023-12-17 15:47:49 +0900

    Example using ipc with renderer

--- renderer.js
+++ renderer.js
@@ -2,3 +2,10 @@ const information = document.getElementById('info');
 
 // eslint-disable-next-line no-undef
 information.innerText = `This app is using Chrome (v${versions.chrome()})`;
+
+const func = async () => {
+	const response = await window.versions.ping();
+	console.log(response);
+};
+
+func();
