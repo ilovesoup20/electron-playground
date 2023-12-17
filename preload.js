97fd94ef32c1570b18b305052ae554e2830cea82
Charles Chong <ilovesoup@fastmail.com>
2023-12-17 15:44:25 +0900

    Example using renderer.js

--- preload.js
+++ preload.js
@@ -1,3 +1,11 @@
+const { contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('versions', {
+	node: () => process.versions.node,
+	chrome: () => process.versions.chrome,
+	electron: () => process.versions.electron
+});
+
 window.addEventListener('DOMContentLoaded', () => {
 	const replaceText = (selector, text) => {
 		const element = document.getElementById(selector);
---
+++ renderer.js
@@ -0,0 +1,4 @@
+const information = document.getElementById('info');
+
+// eslint-disable-next-line no-undef
+information.innerText = `This app is using Chrome (v${versions.chrome()})`;
